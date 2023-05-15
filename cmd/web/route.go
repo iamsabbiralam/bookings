7c2d8ee1ed5e93a3dda2b6e45972674d2021e738
iamsabbiralam <sabbiriph4@gmail.com>
2023-05-16 00:56:32 +0600

    create static folder
    1. create html template
    2. create landing page
    3. create reservation page

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -20,5 +20,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/")) // dot is root folder and static is folder name where all static files are stored
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
