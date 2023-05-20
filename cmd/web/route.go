6adab3246b2480eac233e85b5e15cdc7fd48358d
iamsabbiralam <sabbiriph4@gmail.com>
2023-05-20 14:06:05 +0600

    create handles for forms and adding csrf protection
    1. create handles for forms and adding csrf protection
    2. creating a handler that return json

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -21,7 +21,12 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/about", handlers.Repo.About)
 	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suit", handlers.Repo.Majors)
+
+	// search availability
 	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
+
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Get("/contact", handlers.Repo.Contact)
 
